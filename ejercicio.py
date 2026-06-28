a4919148c2b1fa8e540be830d2040f845bb6eda2
Leenify <nanisk698@gmail.com>
2026-06-28 12:41:33 -0600

    Refactor password management and add password generator

--- ejercicio.py
+++ ejercicio.py
@@ -1,13 +1,24 @@
+import random
 usuarios={
     "Admin":"admin00",
     "Kari":"123"
 }
+
+def conau():
+    caracteres="+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    longitud=int(input("Introduce la longitud de tu contraseña: "))
+    cge=""
+    for i in range(longitud):
+        cge+=random.choice(caracteres)
+    print("Contraseña generada:",cge)
+    return cge
+
 ver=input("Elige un modo: 0-crear contraseña neuva, 1-Ingresar contraseña existente:\n")
 while ((ver!='0') and (ver!='1')):
     ver=input("Símbolo no válido. Elija 0 o 1. (0 crear contraseña, mientras que 1 permite el acceso) \n")
 
 if  ver=="1":
-    usu=input("Ingresa el nombre de usuario")
+    usu=input("Ingresa el nombre de usuario: ")
     if usu not in usuarios:
         print("Usuario no encontrado")
     else:
@@ -16,11 +27,12 @@ if  ver=="1":
             print("¡Contraseña incorrecta!")
         else:
             print("Bienvenido",usu)
+            print(usuarios)
     
 else:
     print("Se requiere autorización del administrador")
     adm=input("Ingrese usuario del administrador: ")
-    if adm!="Admin":
+    if adm!=list(usuarios.keys())[0]:
         print("Acceso denegado")
     else:
         pas=input("Ingresa la contraseña: ")
@@ -28,8 +40,14 @@ else:
             print("Contraseña incorrecta")
         else:
             nueusu=input("Escriba el nuevo usuario: ")
-            nuecontra=input("Escribe la nueva contraseña: ")
+            ocon=input("¿Quieres generar una contraseña automática? 0-No, 1-Sí:\n")
+            while ocon!="0" and ocon!="1":
+                ocon=input("Opción no válida. Elige 0 o 1: ")
+            if ocon=="1":
+                nuecontra=conau()
+            else:
+                nuecontra=input("Escribe la nueva contraseña: ")
             if len(nuecontra) > 0 and len(nueusu) > 0:
                 usuarios[nueusu]=nuecontra
-                print("¡Tu contraseña y usuario se ha guardado correctamente!")
+                print("¡Tu usuario y contraseña se ha guardado correctamente!")
                 print(usuarios)
